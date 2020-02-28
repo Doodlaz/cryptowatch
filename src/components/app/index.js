6ca9fb4622e284345c5ce484363f06a4c281e482
Nikita Ryzhov <n.ryzhov@artk.ai>
2020-02-28 18:23:19 +0200

    add markup and logic

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,32 +1,106 @@
-import React, { useEffect } from "react";
-import io from 'socket.io-client';
+import React, { useState } from "react"
+import { format } from "date-fns"
 
-import { Wrap } from './styles'
+//styles
+import { Wrap, Log, Table } from './styles'
 
-const API_KEY = 'XT11G0PE8VX02ZZ49RUR';
-const uri = 'wss://stream.cryptowat.ch/connect?apikey='+API_KEY;
-
-
-const socket = io(uri);
+//web socket
+const socket = new WebSocket('wss://stream.cryptowat.ch/connect?apikey=9YFF48Q8AIBBN6KM1YIX')
 
 const App = () => {
 
+  const [loading, setloading] = useState(true)
+  const [log, setLog] = useState([])
+  const [data, setData] = useState([])
 
-  useEffect(() => {
+  const subscribeTo = (socket, res) => {
+    socket.send(JSON.stringify({
+      subscribe: {
+        subscriptions: res.map( res => ({ streamSubscription: { resource: res } }))
+      }
+    }))
+  }
 
-    socket.on('message', payload => {
-      console.log({ payload })
-    });
-  });
+  const formatData = data => {
+    if (data.marketUpdate && data.marketUpdate.tradesUpdate) {
+      for (let trade of data.marketUpdate.tradesUpdate.trades) {
+        const id = data.marketUpdate.market.marketId
+        const date = format(new Date(trade.timestampNano/1000000), 'iii, dd MMM yyyy HH:mm:ss')
+        const price = trade.priceStr
+        const amount = trade.amountStr
+        const newData = { // new data row
+          id: id,
+          price: price,
+          amount: amount
+        }
 
-  console.log(socket);
+        setData(oldData =>
+          [newData, ...(oldData.length < 10 ? oldData : oldData.splice(0, oldData.length - 1))]
+        ) //add table row to data array
 
-  return (
+        setLog(oldLog =>
+          [`BTC/USD trade on market ${id}: ${date} GMT ${price} USD ${amount} btc`, ...oldLog]
+        ) //add new log to log array
+      }
+    }
+  }
+
+  const blobAsText = (e, reader) => {
+    const data = JSON.parse(e.srcElement.result.toString())
+
+    formatData(data)
+
+    return reader.removeEventListener('loadend', blobAsText)
+  }
+
+  socket.onopen = () => {
+    subscribeTo(socket, ['instruments:9:trades'])
+  }
+
+  socket.onmessage = (msg) => {
+    const reader = new FileReader()
+
+    setloading(false)
+
+    reader.readAsText(msg.data)
+
+    reader.addEventListener('loadend', e => blobAsText(e, reader))
+  }
+
+  return loading
+    ?
+      <img className={'loader'} src={'https://clipartmag.com/assets/img/loading.gif'} />
+    :
     <Wrap>
-      qwe
+      <Table>
+        <div className={ 'head' }>
+          <div className={ 'td td_id' }>ID</div>
+
+          <div className={ 'td' }>Price</div>
+
+          <div className={ 'td' }>Amount BTC</div>
+        </div>
+
+        <div className={ 'body' }>
+          { data && data.map(( item, index ) => (
+            <div key={ index } className={ 'tr' }>
+              <div className={ 'td td_id' }>{ item.id }</div>
+
+              <div className={ 'td' }>{ item.price }</div>
+
+              <div className={ 'td' }>{ item.amount }</div>
+            </div>
+          ))}
+        </div>
+
+      </Table>
+      <Log>{ log && log.map(( item, index ) => (<p key={ index }>{ item }</p>))}</Log>
     </Wrap>
-  )
+
 }
 export default App
+
+
+
 
 
--- src/components/app/styles.js
+++ src/components/app/styles.js
@@ -1,5 +1,70 @@
 import styled from 'styled-components'
 
 export const Wrap = styled.div`
- 
+  display: flex;
+  width: 100%;
+  height: 60vh;
+  max-width: 1200px;
+  margin: auto;
+  justify-content: space-between;
+  padding: 0 24px;
+`
+
+export const Table = styled.div`
+  flex: 0 0 40%;
+  max-width: 40%;
+  height: 100%;
+  background: #fff;
+  border: 1px solid #555;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  
+  .head {
+    display: flex;
+    height: 60px;
+    
+    .td {
+      font-weight: 600;
+      background: #dedede;
+    }
+  }
+  
+  .body {
+    height: 100%;
+  }
+  
+  .td {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 0 8px;
+       
+    &_id {
+      width: 150px;
+    }
+  }
+  .tr {
+    display: flex;
+    height: 10%;
+    
+    &:nth-of-type(even) {
+      background: #eee;
+    }
+    
+  }
+`
+export const Log = styled.div`
+  flex: 0 0 58%;
+  max-width: 58%;
+  height: 100%;
+  overflow: auto;
+  background: #000;
+  color: green;
+  padding: 0 16px;
+  
+  p {
+    font-size: 12px;
+    font-weight: 300; 
+  }
 `
